820d2bb38bfee9623d79e195ae5cf9268aa5a2d9
CampoDiFiori <dudkopiotr@gmail.com>
2021-09-26 21:51:32 +0200

    Refactor caching points, add reading endpoint from CL

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Copy, Debug)]
 enum FieldState {
+    Unhandled,
     InPriorityQueue,
     Handled,
 }
@@ -17,27 +18,22 @@ enum Direction {
     Right,
 }
 
-impl Direction {
-    fn opposite(&self) -> Direction {
-        match self {
-            Direction::Up => Direction::Down,
-            Direction::Down => Direction::Up,
-            Direction::Left => Direction::Right,
-            Direction::Right => Direction::Left,
-        }
-    }
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Debug, Serialize, Deserialize)]
-struct Point {
+#[derive(Deserialize, Debug)]
+struct DeserializablePoint {
     x: usize,
     y: usize,
     finish: Option<bool>,
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Debug, Serialize)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
 impl Point {
     fn new(x: usize, y: usize) -> Self {
-        Self { x, y, finish: None }
+        Self { x, y }
     }
 }
 
@@ -51,6 +47,8 @@ struct MazeField {
 struct Maze {
     points: HashMap<Point, MazeField>,
     queue: VecDeque<Point>,
+    endpoint: String,
+    finish_point: Option<Point>,
 }
 
 impl MazeField {
@@ -64,23 +62,20 @@ impl MazeField {
 }
 
 impl Maze {
-    fn new() -> Self {
+    fn new(endpoint: String) -> Self {
         Self {
             points: HashMap::new(),
             queue: VecDeque::new(),
+            endpoint,
+            finish_point: None,
         }
     }
 
     fn relax_neighbor(&mut self, curr_point: Point, curr_field: MazeField, direction: Direction) {
-        let Point { x, y, finish } = curr_point;
+        let Point { x, y } = curr_point;
 
         let neighboring_point = match direction {
-            Direction::Down => {
-                if y == 7 {
-                    return;
-                }
-                Point::new(x, y + 1)
-            }
+            Direction::Down => Point::new(x, y + 1),
             Direction::Up => {
                 if y == 0 {
                     return;
@@ -93,15 +88,8 @@ impl Maze {
                 }
                 Point::new(x - 1, y)
             }
-            Direction::Right => {
-                if x == 8 {
-                    return;
-                }
-                Point::new(x + 1, y)
-            }
+            Direction::Right => Point::new(x + 1, y),
         };
-
-        let neighboring_field = self.points.get_mut(&neighboring_point);
 
         let turns = if curr_field.direction == direction {
             curr_field.turns
@@ -109,30 +97,34 @@ impl Maze {
             curr_field.turns + 1
         };
 
-        if let Some(neighboring_field) = neighboring_field {
+        if let Some(neighboring_field) = self.points.get_mut(&neighboring_point) {
             match neighboring_field.state {
-                FieldState::Handled => {}
+                FieldState::Unhandled => {
+                    neighboring_field.turns = turns;
+                    neighboring_field.direction = direction;
+                    neighboring_field.state = FieldState::InPriorityQueue;
+                    self.queue.push_back(neighboring_point);
+                }
                 FieldState::InPriorityQueue => {
                     if turns < neighboring_field.turns {
                         neighboring_field.turns = turns;
                         neighboring_field.direction = direction;
                     }
                 }
+                FieldState::Handled => {}
             }
-        } else if MAZE[neighboring_point.x][neighboring_point.y] == 1 {
-            let unhandled_neighbor = MazeField::new(turns, direction, FieldState::InPriorityQueue);
-            self.queue.push_back(neighboring_point);
-            self.points.insert(neighboring_point, unhandled_neighbor);
         }
     }
 
-    fn relax_field(&mut self) -> Option<Point> {
+    fn pop_queue_front_field(&mut self) -> Option<Point> {
         let curr_point = self.queue.pop_front()?;
         let curr_field = {
             let curr_field = self.points.get_mut(&curr_point)?;
             curr_field.state = FieldState::Handled;
             *curr_field
         };
+
+        self.cache_neighbors(curr_point);
 
         [
             Direction::Right,
@@ -141,90 +133,70 @@ impl Maze {
             Direction::Down,
         ]
         .iter()
-        // .filter(|&&direction| direction != curr_point.get().direction.opposite())
         .for_each(|&direction| self.relax_neighbor(curr_point, curr_field, direction));
 
         Some(curr_point)
     }
+
+    fn cache_neighbors(&mut self, point: Point) {
+        use websocket::{ClientBuilder, Message, OwnedMessage};
+        let mut client = ClientBuilder::new(&self.endpoint)
+            .unwrap()
+            .connect_insecure()
+            .unwrap();
+
+        client
+            .send_message(&Message::text(serde_json::to_string(&point).unwrap()))
+            .unwrap();
+
+        let response = client.recv_message().unwrap();
+
+        if let OwnedMessage::Text(response) = response {
+            let neighbors: Vec<DeserializablePoint> = serde_json::from_str(&response).unwrap();
+            neighbors.iter().for_each(|neighboring_point| {
+                let point = Point::new(neighboring_point.x, neighboring_point.y);
+                if self.points.get(&point).is_none() {
+                    self.points.insert(
+                        point,
+                        MazeField::new(usize::MAX, Direction::Up, FieldState::Unhandled),
+                    );
+                }
+
+                if neighboring_point.finish.is_some() {
+                    self.finish_point = Some(point);
+                }
+            })
+        } else {
+            panic!("Reveived a malformed response");
+        }
+    }
 }
 
-// const MAZE: [[u8; 9]; 8] = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [1, 1, 1, 1, 0, 1, 1, 1, 0],
-//     [0, 1, 0, 1, 0, 1, 0, 1, 0],
-//     [0, 1, 0, 1, 1, 1, 0, 1, 0],
-//     [0, 1, 0, 0, 0, 0, 0, 1, 0],
-//     [0, 1, 1, 1, 1, 0, 0, 1, 0],
-//     [0, 1, 0, 0, 1, 1, 1, 1, 1],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-// ];
-
-const MAZE: [[u8; 8]; 9] = [
-    [0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 1, 1, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 1, 1, 0],
-    [0, 1, 1, 1, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0, 0, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0],
-];
-
 fn main() {
-    let mut maze = Maze::new();
+    let args: Vec<String> = std::env::args().collect();
+    let endpoint = args[1].clone();
+
+    let mut maze = Maze::new(endpoint);
 
     let starting_point = Point::new(0, 1);
     let starting_field = MazeField::new(0, Direction::Right, FieldState::InPriorityQueue);
 
     maze.points.insert(starting_point, starting_field);
-
-    //   ws://rekrutacja.westeurope.cloudapp.azure.com/maze1
-    //   ws://rekrutacja.westeurope.cloudapp.azure.com/maze2
-    //   ws://rekrutacja.westeurope.cloudapp.azure.com/maze3
-    //   ws://rekrutacja.westeurope.cloudapp.azure.com/maze4
-
     maze.queue.push_back(starting_point);
 
-    while let Some(point) = maze.relax_field() {
-        println!("{:?}", point);
-    }
-
-    // println!("{:?}", maze.points.get(&Point::new(6, 8)));
-
-    for (i, row) in MAZE.iter().enumerate() {
-        for (j, _) in row.iter().enumerate() {
-            if let Some(field) = maze.points.get(&Point::new(i, j)) {
-                print!("{} ", field.turns);
-            } else {
-                print!("- ");
-            }
+    while let Some(point) = maze.pop_queue_front_field() {
+        println!("Popped {:?} off queue", point);
+        if let Some(true) = maze.finish_point.map(|finish_point| finish_point.x == point.x && finish_point.y == point.y) {
+            println!(
+                "Number of least turns at exit: {:?}",
+                maze.points
+                    .get(&point)
+                    .map(|exit_field| exit_field.turns)
+                    .expect("Finish field did not show up")
+            );
+            return;            
         }
-        println!();
     }
 
-    check_neighbors(Point::new(0, 1));
-}
-
-fn check_neighbors(point: Point) -> Vec<Point> {
-    use websocket::{ClientBuilder, Message, OwnedMessage};
-    let mut client = ClientBuilder::new("ws://rekrutacja.westeurope.cloudapp.azure.com/maze2")
-        .unwrap()
-        .connect_insecure()
-        .unwrap();
-
-    client
-        .send_message(&Message::text(serde_json::to_string(&point).unwrap()))
-        .unwrap();
-
-    let response = client.recv_message().unwrap();
-
-    if let OwnedMessage::Text(response) = response {
-        println!("{}", response);
-        let neighbors: Vec<Point> = serde_json::from_str(&response).unwrap();
-        println!("{:?}", neighbors);
-        return neighbors;
-    }
-
-    panic!("Couldnt send a ws message");
+    println!("Finish point did not show up");
 }
